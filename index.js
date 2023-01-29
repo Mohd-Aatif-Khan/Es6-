abadcafc5934cf03d0ef2f6807b7f271997591a8
Mohd Aatif Khan <114300008+Mohd-Aatif-Khan@users.noreply.github.com>
2023-01-29 13:35:39 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+let num1;
+const num2 =10;
+
+const obj1={
+name:"jot"
+};
+console.log(obj1.name)
+
+obj1.name ="chandler"
+console.log(obj1.name)
+
+
+// arrow functions
+const getarrow =(m)=>10*m;
+console.log(getarrow(5))
+
